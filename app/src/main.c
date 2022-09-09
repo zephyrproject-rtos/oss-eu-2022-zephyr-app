1f49572b40e62826f04a9e5be6554db68f169427
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2022-09-09 18:59:04 +0200

    app: remove example sensor code
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- app/src/main.c
+++ app/src/main.c
@@ -4,53 +4,19 @@
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
 #include <zephyr/usb/usb_device.h>
-
-#include "app_version.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
 void main(void)
 {
-	int ret;
-	const struct device *sensor;
-
-	printk("Zephyr Example Application %s\n", APP_VERSION_STR);
-
 #ifdef CONFIG_USB_DEVICE_STACK
-	ret = usb_enable(NULL);
+	int ret = usb_enable(NULL);
 	if (ret < 0) {
 		LOG_ERR("Failed to enable USB");
 		return;
 	}
 #endif
-
-	sensor = DEVICE_DT_GET(DT_NODELABEL(examplesensor0));
-	if (!device_is_ready(sensor)) {
-		LOG_ERR("Sensor not ready");
-		return;
-	}
-
-	while (1) {
-		struct sensor_value val;
-
-		ret = sensor_sample_fetch(sensor);
-		if (ret < 0) {
-			LOG_ERR("Could not fetch sample (%d)", ret);
-			return;
-		}
-
-		ret = sensor_channel_get(sensor, SENSOR_CHAN_PROX, &val);
-		if (ret < 0) {
-			LOG_ERR("Could not get sample (%d)", ret);
-			return;
-		}
-
-		printk("Sensor value: %d\n", val.val1);
-
-		k_sleep(K_MSEC(1000));
-	}
 }
 
